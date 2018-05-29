817fb2042181ff8d30f69ecc749c2df5159578b6
Dencel K Babu <dencelkbabu007@gmail.com>
2018-05-29 18:17:34 +0530

    santoni: Omni-fy

--- omni_santoni.mk
+++ omni_santoni.mk
@@ -16,10 +16,13 @@
 
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common OmniRom stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-PRODUCT_NAME := lineage_santoni
+PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/CarrierConfig
+
+PRODUCT_NAME := omni_santoni
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
